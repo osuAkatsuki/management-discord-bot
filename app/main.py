883706459960da0678eaebb305e0089bb0014619
cmyui <cmyuiosu@gmail.com>
2023-05-22 13:30:27 -0400

    use db ca certificates for ssl

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
-import discord
-import sys
+import base64
 import os
+import ssl
+import sys
 
+import discord
 from discord.ext import commands
 
 # add .. to path
 srv_root = os.path.join(os.path.dirname(__file__), "..")
+
 sys.path.append(srv_root)
 
 from app import settings
@@ -22,7 +25,7 @@ class Bot(commands.Bot):
             command_prefix=commands.when_mentioned_or("!"),
             help_command=None,
             *args,
-            **kwargs
+            **kwargs,
         )
 
     async def setup_hook(self) -> None:
@@ -46,7 +49,14 @@ async def on_ready() -> None:
             port=settings.READ_DB_PORT,
             database=settings.READ_DB_NAME,
         ),
-        db_ssl=settings.READ_DB_USE_SSL,
+        db_ssl=(
+            ssl.create_default_context(
+                purpose=ssl.Purpose.SERVER_AUTH,
+                cadata=base64.b64decode(settings.READ_DB_CA_CERTIFICATE).decode(),
+            )
+            if settings.READ_DB_USE_SSL
+            else False
+        ),
         min_pool_size=settings.DB_POOL_MIN_SIZE,
         max_pool_size=settings.DB_POOL_MAX_SIZE,
     )
@@ -61,7 +71,14 @@ async def on_ready() -> None:
             port=settings.WRITE_DB_PORT,
             database=settings.WRITE_DB_NAME,
         ),
-        db_ssl=settings.WRITE_DB_USE_SSL,
+        db_ssl=(
+            ssl.create_default_context(
+                purpose=ssl.Purpose.SERVER_AUTH,
+                cadata=base64.b64decode(settings.WRITE_DB_CA_CERTIFICATE).decode(),
+            )
+            if settings.WRITE_DB_USE_SSL
+            else False
+        ),
         min_pool_size=settings.DB_POOL_MIN_SIZE,
         max_pool_size=settings.DB_POOL_MAX_SIZE,
     )
